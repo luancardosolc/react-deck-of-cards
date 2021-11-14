cf2bfa94bbfa16aaa3031a6f1c0995dd1f816ca6
Luan Cardoso <luanleitecardoso@gmail.com>
2021-11-14 20:43:42 -0300

    Sytling rotation card elements

--- src/components/new-deck.tsx
+++ src/components/new-deck.tsx
@@ -10,9 +10,16 @@ function NewDeck() {
       <div className="table">
         <div className="add_card">
           <label htmlFor="card_input">Add cards to the pile</label>
-          <input type="text" name="card_input"/>
+          <input type="text" name="card_input" />
           <button>Add</button>
         </div>
+      </div>
+      <div className="rotation_card">
+        <label htmlFor="rotation_card_input">Rotation card</label>
+      </div>
+      <div className="rotation_card_body">
+        <input type="text" name="rotation_card_input" placeholder="Card name" />
+        <button>Submit Deck</button>
       </div>
     </div>
   );
