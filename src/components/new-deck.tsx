8f0f9adb846d09a7ba6d15b1c5278533dd45f847
Luan Cardoso <luanleitecardoso@gmail.com>
2021-11-15 09:45:39 -0300

    Add valid cards to the pile variable

--- src/components/new-deck.tsx
+++ src/components/new-deck.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 
 function NewDeck() {
-  const notify = (msg: string) => toast.success(msg);
+  const notifySuccess = (msg: string) => toast.success(msg);
+  const notifyError = (msg: string) => toast.error(msg);
+
+  //Strongest to weakest
+  const originalSuits = ['H', 'D', 'C', 'S'];
+  const originalValues = ['2', 'A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3'];
+  const [pile, setPile] = useState<string[]>([]);
+  const [cardInput, setCardInput] = useState('');
+  const handleChange = (e: { target: { value: React.SetStateAction<string>; }; }) => setCardInput(e.target.value);
+
+  const addCardToPile = () => {
+    const [value, suit] = cardInput.split('');
+    const findSuit = originalSuits.includes(suit);
+    const findValue = originalValues.includes(value);
+    const findPile = pile.includes(cardInput);
+
+    if (!(findSuit && findValue)) {
+      notifyError('Invalid card!');
+      return;
+    }
+
+    if (findPile) {
+      notifyError('The pile already contains this card!');
+      return;
+    }
+
+    setPile([...pile, cardInput]);
+    console.log('pile', pile);
+    notifySuccess(`Card '${cardInput}' added to the pile.`);
+  }
 
   return (
     <div>
@@ -37,8 +66,8 @@ function NewDeck() {
       <div className="table table-controls">
         <div className="add_card">
           <label htmlFor="card_input">Add cards to the pile</label>
-          <input type="text" name="card_input" />
-          <button>Add</button>
+          <input type="text" name="card_input" maxLength={2} value={cardInput} onChange={handleChange} />
+          <button onClick={addCardToPile}>Add</button>
         </div>
       </div>
       <div className="rotation_card">
